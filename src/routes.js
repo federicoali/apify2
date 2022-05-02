ab7771793b9da5819c3253a8ddfca907fffd8c31
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-02 16:37:36 +0200

    update

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
     await page.waitForSelector('div.sg-product__dpdp-c');
 
     const resultsLength = await page.evaluate(() => {
-        return document.querySelector('div.sh-pr__product-results').children.length;
+        return document.querySelector('div.sh-osd__offer-row').children.length;
     });
 
 
