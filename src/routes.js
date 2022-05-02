cfa5453e73830297428f46797be6fb45c52c7ced
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-02 16:54:18 +0200

    update

--- src/routes.js
+++ src/routes.js
@@ -46,13 +46,13 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
                 // Please pay attention that "merchantMetrics" and "reviewsLink" were removed from the  "SEARCH" page.
                 const item = results[i];
                 // KEYS OF OUTPUT OBJ
-                const company = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td:nth-child(1) > div.kPMwsc > a");
+                const company = document.querySelector("a.b5ycib");
 
-                const price = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td:nth-child(3) > span");
+                const price = document.querySelector("g9WBQb");
 
-                const details = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td.SH30Lb.yGibJf > div");
+                const details = document.querySelector("td.SH30Lb");
 
-                const total = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td:nth-child(4) > div > div.drzWO");
+                const total = document.querySelector("drzWO");
 
                 // FINAL OUTPUT OBJ
                 const output = {
