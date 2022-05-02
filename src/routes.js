40ade854c0b1f5376ea0657450afe6f99770483b
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-02 16:28:43 +0200

    update

--- src/routes.js
+++ src/routes.js
@@ -10,23 +10,6 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
     let { savedItems, pageNumber } = request.userData;
     const { hostname } = request.userData;
 
-    await page.waitForSelector('div.sg-product__dpdp-c');
-
-    const resultsLength = await page.evaluate(() => {
-        return document.querySelector('sh-osd__online-sellers-cont').children.length;
-    });
-    
-
-
-    // check HTML if page has no results
-    if (resultsLength === 0) {
-        log.warning('The page has no results. Check dataset for more info.');
-
-        await Apify.pushData({
-            '#debug': Apify.utils.createRequestDebugInfo(request),
-        });
-    }
-
 
     log.info(`Found ${resultsLength} products on the page.`);
     // eslint-disable-next-line no-shadow
@@ -43,47 +26,25 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
             // eslint-disable-next-line no-shadow
             const data = [];
             // ITERATING NODES TO GET RESULTS
-            for (let i = 0; i < results.length; i++) {
+            for (let i = 0; i < 20; i++) {
                 // Please pay attention that "merchantMetrics" and "reviewsLink" were removed from the  "SEARCH" page.
                 const item = results[i];
                 // KEYS OF OUTPUT OBJ
-                const title = item.querySelector('h3') ? item.querySelector('h3') : null;
+                const company = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td:nth-child(1) > div.kPMwsc > a");
 
-                const productName = title?.textContent ?? null;
+                const price = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td:nth-child(3) > span");
 
-                const productLinkAnchor = item.querySelector('a[href*="shopping/product/"]')
-                    ? item.querySelector('a[href*="shopping/product/"]')
-                    : null;
-                const productLink = productLinkAnchor ? productLinkAnchor.href : null;
+                const details = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td.SH30Lb.yGibJf > div");
 
-                const price = item.querySelector('div[data-sh-or="price"] div > span > span')?.textContent ?? null;
-
-                const description = item.querySelectorAll('div.hBUZL')[1]?.textContent ?? null;
-
-                const merchantName = item.querySelector('div[data-sh-or="price"]')?.nextSibling?.textContent ?? null;
-
-                const merchantLink = item.querySelector('div[data-sh-or="price"]')?.parentElement?.parentElement?.href ?? null;
-
-                const idArray = productLink ? productLink.split('?')[0].split('/') : null;
-                const shoppingId = idArray ? idArray[idArray.length - 1] : null;
-
-                const reviewsScore = item.querySelector('div[aria-label*="product reviews"] span')?.textContent ?? null;
-                const reviewsCount = item.querySelector('div[aria-label*="product reviews"]')
-                    ? item.querySelector('div[aria-label*="product reviews"]').getAttribute('aria-label').split(' ')[0]
-                    : null;
+                const total = document.querySelector("#sh-osd__online-sellers-cont > tr:nth-child(1) > td:nth-child(4) > div > div.drzWO");
 
                 // FINAL OUTPUT OBJ
                 const output = {
                     query,
-                    productName,
-                    productLink,
+                    company,
+                    details,
                     price,
-                    description,
-                    merchantName,
-                    merchantLink,
-                    shoppingId,
-                    reviewsScore,
-                    reviewsCount,
+                    total,
                     positionOnSearchPage: i + 1,
                     productDetails: item.querySelectorAll('.translate-content')[1]?.textContent.trim(),
                 };
