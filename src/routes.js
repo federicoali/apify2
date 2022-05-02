77eee0995f3e6978b80c7f4d255444accfe0b0cb
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-02 17:41:22 +0200

    update

--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,7 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
     const data = await page.evaluate(
         (maxPostCount, query, savedItems) => {
             // nodes with items
-            let results = Array.from(document.getElementById('sh-osd__online-sellers-cont'));
+            let results = Array.from(document.querySelectorAll('sh-osd__offer-row'));
             // limit the results to be scraped, if maxPostCount exists
             console.log('result', results);
             // eslint-disable-next-line no-shadow
