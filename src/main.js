7bc1675178c9f1c75b3c32bd35811d5639faae25
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-02 11:37:11 +0200

    update

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ const { log } = Apify.utils;
 Apify.main(async () => {
     const input = await Apify.getValue('INPUT');
 
+    module.exports = {
+        input
+    };
+
     // Validate the input
     if (!input) throw new Error('Missing configuration');
 
