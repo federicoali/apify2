55555e532115ae40cf8e591448cd05c7f0e648b0
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-02 15:41:47 +0200

    update

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ exports.SEARCH_PAGE = async (page, request, query, requestQueue, maxPostCount, e
     let { savedItems, pageNumber } = request.userData;
     const { hostname } = request.userData;
 
-    await page.getElementById('d0wB0c');
+    await page.waitForSelector('d0wB0c');
 
 
     // eslint-disable-next-line no-shadow
