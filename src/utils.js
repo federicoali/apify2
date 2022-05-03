0ed23a41b73471ca6ff42b2875f15cb6436b7eaf
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-03 17:16:58 +0200

    update

--- src/utils.js
+++ src/utils.js
@@ -55,9 +55,9 @@ function countryCodeToGoogleHostname(countryCode) {
 }
 
 // New function which forms a URL from countryCode and query params
-function formUrl(countryCode, query) {
+function formUrl(countryCode, inputUrl) {
     const hostname = countryCodeToGoogleHostname(countryCode);
-    const url = query;
+    const url = inputUrl;
     return { url, hostname };
 }
 
