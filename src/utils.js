66cc3978254f7a4127ebe6276a7111f7428dc92f
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-04 09:34:12 +0200

    update

--- src/utils.js
+++ src/utils.js
@@ -73,10 +73,12 @@ async function makeRequestList(queries, inputUrl, countryCode) {
         sources = startUrls.map((startUrl) => {
             // URL has to start with plain http for SERP proxy to work
             let { url } = startUrl;
+            if (url.startsWith('https')) {
+                url = url.replace('https', 'http');
+            }
 
-
-            if (url.startsWith('https://google')) {
-                url = url.replace('https://google', 'https://www.google.it');
+            if (url.startsWith('http://google')) {
+                url = url.replace('http://google', 'http://www.google.it');
             }
 
             return new Apify.Request({
