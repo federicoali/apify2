882fb8f0f33911006ee59faad91bb3a8e4ffeb0a
Nooo Agency <filippo@MBP-di-Bruno-O.wind3.hub>
2022-05-05 17:29:39 +0200

    update

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ Apify.main(async () => {
 
     const proxyConfiguration = await Apify.createProxyConfiguration({
         groups: ['GOOGLE_SERP'],
-        countryCode: 'IT',
+        countryCode: 'it',
     });
 
     // crawler config
